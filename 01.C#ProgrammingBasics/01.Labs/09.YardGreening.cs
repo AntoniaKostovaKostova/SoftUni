4c28e89c01b82256453d8d19880fc60078357e48
AntoniaKostovaKostova <76847367+AntoniaKostovaKostova@users.noreply.github.com>
2021-01-01 20:48:50 +0200

    Add

---
+++ 01.C#ProgrammingBasics/01.Labs/09.YardGreening.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareMeters = double.Parse(Console.ReadLine());
+            double price = squareMeters * 7.61;          
+            double discount = price * 0.18;
+            double priceWithDiscount = price - discount;
+            Console.WriteLine($"The final price is: {priceWithDiscount} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
