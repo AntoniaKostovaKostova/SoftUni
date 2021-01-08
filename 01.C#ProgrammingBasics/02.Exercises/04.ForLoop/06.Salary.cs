475cc80687b3e69cf4570fd5c7a16d3ee7ea14c7
AntoniaKostovaKostova <76847367+AntoniaKostovaKostova@users.noreply.github.com>
2021-01-08 21:40:11 +0200

    Create 06.Salary.cs

---
+++ 01.C#ProgrammingBasics/02.Exercises/04.ForLoop/06.Salary.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Ex06Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= countTabs; i++)
+            {
+                string tab = Console.ReadLine();
+                switch (tab)
+                {
+                    case "Facebook":
+                        salary -= 150;
+                        break;
+                    case "Instagram":
+                        salary -= 100;
+                        break;
+                    case "Reddit":
+                        salary -= 50;
+                        break;
+                }
+                if (salary <= 0)
+                {
+                    i = countTabs;
+                    Console.WriteLine("You have lost your salary.");
+                }
+            }
+            if (salary > 0)
+            {
+                Console.WriteLine($"{salary}");
+            }
+
+        }
+    }
+}
