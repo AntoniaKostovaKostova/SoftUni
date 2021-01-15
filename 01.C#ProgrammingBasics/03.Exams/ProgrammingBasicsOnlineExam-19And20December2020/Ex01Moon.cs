98414344994cb7d5d997eec0ebd4454b49214f78
AntoniaKostovaKostova <76847367+AntoniaKostovaKostova@users.noreply.github.com>
2021-01-15 22:05:13 +0200

    Create Ex01Moon.cs

---
+++ 01.C#ProgrammingBasics/03.Exams/ProgrammingBasicsOnlineExam-19And20December2020/Ex01Moon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex01Moon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double speed = double.Parse(Console.ReadLine());
+            double fuelPerHundredKilometers = double.Parse(Console.ReadLine());
+            double distance = 384400;
+
+            double distanceInTwoDirections = distance * 2;
+
+            double hours = Math.Ceiling(distanceInTwoDirections / speed) + 3;
+
+            double sumFuel = ((distance * 2) * fuelPerHundredKilometers) / 100;
+
+
+            Console.WriteLine(hours);
+            Console.WriteLine(Math.Ceiling(sumFuel));
+
+        }
+    }
+}
