73e8be8510d2d2700a5b84a0560207e47e4f99c9
AntoniaKostovaKostova <76847367+AntoniaKostovaKostova@users.noreply.github.com>
2021-01-21 15:58:53 +0200

    Create 09.PadawanEquipment.cs

---
+++ 02.C#Fundamentals/02.Exercises/02.IntroAndBasicSyntax/09.PadawanEquipment.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Ex09PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double priceSabre = double.Parse(Console.ReadLine());
+            double priceRobe = double.Parse(Console.ReadLine());
+            double priceBelt = double.Parse(Console.ReadLine());
+            double price = 0;
+            int freeBelt = students / 6;
+            price = ((Math.Ceiling(students * 1.1)) * priceSabre) + (students * priceRobe) + ((students - freeBelt) * priceBelt);
+            if (price <= money)
+            {
+                Console.WriteLine($"The money is enough - it would cost {price:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {price - money:F2}lv more.");
+            }
+        }
+    }
+}
