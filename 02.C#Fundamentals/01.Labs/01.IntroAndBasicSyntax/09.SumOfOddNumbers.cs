f181263061e7de2846f9c27d33348966e21207d9
AntoniaKostovaKostova <76847367+AntoniaKostovaKostova@users.noreply.github.com>
2021-01-22 20:47:02 +0200

    Create 09.SumOfOddNumbers.cs

---
+++ 02.C#Fundamentals/01.Labs/01.IntroAndBasicSyntax/09.SumOfOddNumbers.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Ex09SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int l = 1;
+            for (int i = 1; i <= n; i++, l += 2)
+            {
+                Console.WriteLine(l);
+                sum += l;               
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
