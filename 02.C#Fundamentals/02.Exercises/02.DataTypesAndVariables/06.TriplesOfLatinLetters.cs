30ec4be09119aa2e0900be7e64190f27cb26316f
AntoniaKostovaKostova <76847367+AntoniaKostovaKostova@users.noreply.github.com>
2021-01-29 19:18:57 +0200

    Create 06.TriplesOfLatinLetters.cs

---
+++ 02.C#Fundamentals/02.Exercises/02.DataTypesAndVariables/06.TriplesOfLatinLetters.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Ex06TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.WriteLine($"{(char)('a' + i)}{(char)('a' + k)}{(char)('a' + j)}");
+                    }
+                }
+            }
+        }
+    }
+}
