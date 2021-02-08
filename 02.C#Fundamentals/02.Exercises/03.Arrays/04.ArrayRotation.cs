b4f5862cbc7b832b780ee8178b200616f7a5a31c
AntoniaKostovaKostova <76847367+AntoniaKostovaKostova@users.noreply.github.com>
2021-02-08 22:50:54 +0200

    Create 04.ArrayRotation.cs

---
+++ 02.C#Fundamentals/02.Exercises/03.Arrays/04.ArrayRotation.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Ex04ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine()
+                .Split();
+
+            int rotations = int.Parse(Console.ReadLine());
+       
+            for (int i = 0; i < rotations; i++)
+            {
+                string firstElement = array[0];
+
+                for (int j = 1; j < array.Length; j++)
+                {
+                    int prevIndex = j - 1;
+                    array[prevIndex] = array[j];
+
+                }
+                array[array.Length - 1] = firstElement;
+            }
+            foreach (var element in array)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+        
+    }
+}
