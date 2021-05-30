00d17da0a0f3a0c6d9868683196839715caa3896
AntoniaKostovaKostova <76847367+AntoniaKostovaKostova@users.noreply.github.com>
2021-05-30 14:18:54 +0300

    Create 04.EvenTimes.cs

---
+++ 03.C#Advanced/01.Exercises/03.SetsAndDictionariesAdvanced/04.EvenTimes.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace _04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> elements = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!elements.ContainsKey(number))
+                {
+                    elements.Add(number, 0);
+                }
+                elements[number] +=1;
+            }
+            foreach (var element in elements)
+            {
+                if (element.Value % 2 == 0)
+                {
+                    Console.WriteLine(element.Key);
+                    break;
+                }
+            }
+
+        }
+    }
+}
